640a3f039d6b527609d23df41169844c4a609c6a
Valentin-Ledur <valentin.ledur123456789@gmail.com>
2026-03-24 23:02:51 +0100

    Cube test

---
+++ script.js
@@ -0,0 +1,60 @@
+import * as THREE from 'https://esm.sh/three@0.160.0';
+
+import { GLTFLoader } from 'https://esm.sh/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
+
+const container = document.getElementById('canvas-container');
+
+if (container) {
+    const w = container.clientWidth;
+    const h = container.clientHeight;
+
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x333333);
+
+    const camera = new THREE.PerspectiveCamera(60, w / h, 0.1, 1000);
+    camera.position.set(4, 4, 4);
+    camera.lookAt(0, 0, 0);
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(w, h);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    container.appendChild(renderer.domElement);
+
+    const ambient = new THREE.AmbientLight(0xffffff, 1.5);
+    scene.add(ambient);
+    const sun = new THREE.DirectionalLight(0xffffff, 1);
+    sun.position.set(5, 10, 7);
+    scene.add(sun);
+
+    const cube = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshStandardMaterial({ color: "hotpink" })
+    );
+    scene.add(cube);
+
+    const loader = new GLTFLoader();
+    loader.load('model.glb',
+        (gltf) => {
+            scene.remove(cube);
+            scene.add(gltf.scene);
+            console.log("Succès : Modèle GLB affiché");
+        },
+        undefined,
+        (err) => {
+            console.error("Erreur GLTF :", err);
+        }
+    );
+
+    function animate() {
+        requestAnimationFrame(animate);
+        cube.rotation.y += 0.01;
+        renderer.render(scene, camera);
+    }
+    animate();
+
+    window.addEventListener('resize', () => {
+        camera.aspect = container.clientWidth / container.clientHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(container.clientWidth, container.clientHeight);
+    });
+}
